c0cbfe7c235b0a5c83de39d1bdb1cc5c055224c0
Mike <>
2025-06-14 08:17:03 +0900

    fix: Repo name

--- react-page/vite.config.js
+++ react-page/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mikechen-git.github.io/',
+  base: '/OpenAiTx.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
