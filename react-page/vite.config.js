4b97e64d1741305c683146c05d32ff6d5dd8261c
Mike <>
2025-06-14 08:07:56 +0900

    feat: Enable GitHub Pages deployment

--- react-page/vite.config.js
+++ react-page/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/OpenAiTx.github.io/',
+  base: '/mikechen-git.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
